a8f37c24d97fd77e43b4c83c472767cf05fe6aad
Adson Mozart Santos Paixão <adson.santos@biti9.com.br>
2025-07-01 02:34:55 -0300

    feat: create repository to jpa persistence

---
+++ src/main/java/br/com/cadastroalunos/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package br.com.cadastroalunos.repository;
+
+import br.com.cadastroalunos.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
